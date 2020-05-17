79b6338db159601c90fa394f708afb63ea901635
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-17 11:36:14 +0530

    Updates

---
+++ precache-manifest.fcc100f6dccff9bd0db54a75b2a1c87f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566517c012c6ef0b2ed3d6bd5431c27d",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "c2d3eda505e1fb866bcf",
+    "url": "/Todolist/static/css/main.744c3aef.chunk.css"
+  },
+  {
+    "revision": "75461fedd579883cd178",
+    "url": "/Todolist/static/js/2.dcccc36a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Todolist/static/js/2.dcccc36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d3eda505e1fb866bcf",
+    "url": "/Todolist/static/js/main.6a3e5591.chunk.js"
+  },
+  {
+    "revision": "57c8093504600d199eff",
+    "url": "/Todolist/static/js/runtime-main.4c1022bc.js"
+  }
+]);
